f97b92c5144b4792e7d86553b94f4780d8a6aed1
Marc0z <marco9977@gmail.com>
2019-10-28 15:33:02 +0100

    commentary

--- scripts/poetry-command.sh
+++ scripts/poetry-command.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# script usage example:
+#
+# ./poetry-commands - lists all poetry commands
+# ./poetry-commands add <package> - installs <package> and adds it to the poetry.lock / pyproject.toml
+# ./poetry-commands remove <package> - removes <package> from container/OS and from poetry.lock / pyproject.toml
+
 docker exec -w /opt/poetry -it nest-web-main poetry $@
